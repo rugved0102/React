79ab39bf2e91d393ee4191a4f9fd633d8efb9c73
rugved0102 <rugvedjunghare@gmail.com>
2025-03-31 23:13:08 +0530

    math added

--- JavaScript/01_basics/06_num_and_math.js
+++ JavaScript/01_basics/06_num_and_math.js
@@ -2,3 +2,20 @@ const score = 400
 const balance = new Number(100)
 // console.log(balance);
 console.log(balance.toString().length);
+const hundreds = 1000000
+console.log(hundreds.toLocaleString()); // 1,000,000
+console.log(hundreds.toLocaleString('en-IN')); // 10,00,000
+console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
+console.log(Number.MIN_VALUE); // 5e-324
+
+// +++++++++++++++++++++++++++ Math ++++++++++++++++++++++++++++++++
+console.log(Math);
+console.log(Math.PI); // 3.141592653589793
+console.log(Math.E); // 2.718281828459045
+
+console.log(Math.random());
+console.log(Math.random() * 10); // 0-10
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1) + min)); // 10-20
