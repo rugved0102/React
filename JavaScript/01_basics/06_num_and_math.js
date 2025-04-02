315613810c7e39735bf5406a5bcf7744c65df9c9
rugved0102 <rugvedjunghare@gmail.com>
2025-04-02 22:40:05 +0530

    math random

--- JavaScript/01_basics/06_num_and_math.js
+++ JavaScript/01_basics/06_num_and_math.js
@@ -19,3 +19,4 @@ console.log(Math.random() * 10); // 0-10
 const min = 10
 const max = 20
 console.log(Math.floor(Math.random() * (max - min + 1) + min)); // 10-20
+console.log(Math.floor(Math.random() * 10) + 1); // 1-10
