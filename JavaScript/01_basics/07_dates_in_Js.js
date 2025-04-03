386aa064448242ebc456b99c69bba36e979a073c
rugved0102 <rugvedjunghare@gmail.com>
2025-04-03 14:59:35 +0530

    dates in js added

---
+++ JavaScript/01_basics/07_dates_in_Js.js
@@ -0,0 +1,13 @@
+// Dates
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString);
+console.log(myDate.toTimeString());
+console.log(myDate.toLocaleDateString());   
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toJSON); 
+console.log(typeof myDate); // object
+
+let myCreatedDate = new Date(2023,0,23)
+console.log(myCreatedDate.toDateString());
+
