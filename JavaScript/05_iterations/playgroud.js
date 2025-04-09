e786899e95bc8453126ef18ed7af9e52d043d7d1
rugved0102 <rugvedjunghare@gmail.com>
2025-04-09 23:22:36 +0530

    playgroud.js updates

--- JavaScript/05_iterations/playgroud.js
+++ JavaScript/05_iterations/playgroud.js
@@ -20,3 +20,12 @@ const nums = [1,2,3,4,5,6]
 
 const ans = nums.filter((num) => num % 2 == 0).map((num) => num*2)
 console.log(ans);
+
+// reduce
+const arr = [1,2,3]
+const totalSum = arr.reduce( (previousValue, currentValue) => {
+    console.log(`previousValue: ${previousValue}, currentValue: ${currentValue}`);
+    return previousValue + currentValue
+}, 0) // 0 previousValue di hai
+console.log(totalSum);
+
