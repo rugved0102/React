0a66fcc71389747e5e3a73bc1d2377b6f68b93e2
rugved0102 <rugvedjunghare@gmail.com>
2025-04-30 20:10:51 +0530

    functions as objects

--- JavaScript/10_classes_and_oop/Object.js
+++ JavaScript/10_classes_and_oop/Object.js
@@ -1,9 +1,9 @@
 function MultiplyBy5(num) {
-    return num*5
+    return num * 5;
 }
 
 MultiplyBy5.power = 2;
 
-console.log(MultiplyBy5(5));
-console.log(MultiplyBy5.power);
-console.log(MultiplyBy5.prototype);
+console.log(MultiplyBy5(5));        // ✅ 25
+console.log(MultiplyBy5.power);     // ✅ 2
+console.log(MultiplyBy5.prototype); // ✅ {}
