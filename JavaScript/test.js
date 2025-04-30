22009ed60df0a0cee5a54143fef92917c0e62d84
rugved0102 <rugvedjunghare@gmail.com>
2025-04-30 20:05:42 +0530

    truthy and falsy practice

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,14 +1,22 @@
-console.log('Rugved');
-console.log(1+true);
-console.log(+""+1+true);
+// console.log('Rugved');
+// console.log(1+true);
+// console.log(+""+1+true);
 
-const user = {
-    username: "hitesh",
-    price: 999,
-    welcomeMessage: function() {
-        console.log(`${this.username}, welcome to website`);
-        // console.log(this);
-    }
+// const user = {
+//     username: "hitesh",
+//     price: 999,
+//     welcomeMessage: function() {
+//         console.log(`${this.username}, welcome to website`);
+//         // console.log(this);
+//     }
+// };
+// console.log(user.welcomeMessage());
+// // user.welcomeMessage()
+
+const chai = () => {
+    let username = "hitesh";
+    console.log(this);
 };
-console.log(user.welcomeMessage());
-// user.welcomeMessage()
+chai();
+
+console.log('false');
