5ab294d6e21ecb4ede5567ef1cd2e7013eb76359
rugved0102 <rugvedjunghare@gmail.com>
2025-04-30 21:11:27 +0530

    Array.prototype

--- JavaScript/10_classes_and_oop/prototype.js
+++ JavaScript/10_classes_and_oop/prototype.js
@@ -4,4 +4,67 @@
 // If it doesn't find it there, it starts looking in the prototype — the object it's linked to.
 // It keeps going up the chain of prototypes until it finds the property or reaches the end (null).
 
+let myName = "Rugved     "
 
+console.log(myName.length); // 11
+console.log(myName.trim().length); // 6 // low IQ vala kaam
+
+// muze ek method banani hai jis se har string pr me lagau to leading spaces na gin kr true Length return kare
+
+let myHeros = ["thor", "spiderman"]
+let heroPower = {
+    thor : "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function() {
+        console.log(`Spidy power is ${this.spiderman}`)
+    }
+}
+
+console.log(heroPower.thor.length);
+
+Object.prototype.rugved = function() {
+    console.log(`rugved is present in all objects`);
+    
+}
+heroPower.rugved()
+myHeros.rugved()
+
+
+Array.prototype.heyRugved = function() {
+    console.log(`rugved says hello`);
+    
+}
+// 🟢 You added a custom method to all arrays in JavaScript by extending Array.prototype.
+
+// ✅ Result: Any array (like myHeros) can now call .heyRugved() — because it inherits from Array.prototype.
+myHeros.heyRugved()
+
+
+// Inheritance
+
+const User = {
+    name: "rugved",
+    email: "rugved@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false  
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__= User
+
+// Ye upar wala purne jamane me use krte the
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)  
