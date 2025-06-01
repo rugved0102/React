5bfcca42a9e047dade875111e85eba9e3396a5f3
rugved0102 <rugvedjunghare@gmail.com>
2025-06-01 18:56:32 +0530

    react basic hello world project

--- 01vitebasic/src/App.jsx
+++ 01vitebasic/src/App.jsx
@@ -2,13 +2,13 @@ import Chai from "./chai"
 
 function App() {
   // const [count, setCount] = useState(0)
-
+  const username = "Rugved Junghare"
   return (
     <>
-    <h1>Rugved | Vite</h1>
+    <h1> Vite | {username}</h1> 
     <Chai/>
     </>
-    
+    // {username} ye ek evaluated expression hai. Yaha hamne js nahi likhi hai evaluated js likhi hai
     // isme ek hi elem ko return kr sakte so if you want to return more than one things you can wrap it in `fragment` i.e `<>` `</>`
   )
 }
--- 01vitebasic/src/main.jsx
+++ 01vitebasic/src/main.jsx
@@ -1,10 +1,50 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import React from 'react';
 import App from './App.jsx'
+// App to aakhir me ek function hi hai to hum bhi ek function likh sakte hai
+function MyApp() {
+  return(
+    <div>
+      <h1>Hello! This is MyApp()</h1>
+    </div>
 
+    // ye wala syntax easy hai pr js ko ye samaj nahi ata so bundelers use karke html code ko convert karta vo jaise hamne customreact.js me reactElement banaya tha waise
+    // That's why this is .jsx i.e js+html
+  )
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+  }
+
+  const anotherElem = (
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+  )
+
+  const anotherUser = 'Rugved Junghare'
+
+  const reactElem = React.createElement(
+    'a',
+    {href:'https://google.com',target:'_blank'},
+    'Click me to visit Google',
+    // sab complete hone ke badd yaha variables inject honge
+    anotherUser
+  )
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    
+    // <MyApp />
+     // MyApp() // ye to aakhir me func hi hai na so ye bhi work karega pr yesa nhi karna hai
+
+    //  reactElement // this will not work becoz react me obj ya function ko dom tree me convert karte hai so ye apna custom func hai to ye nhi chalega
+
+    //  anotherElem // ye chal jayega kyuki iska tree ban sakta hai
+
+    reactElem
 )
