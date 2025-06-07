8eb0e5b0a573b42414e54eaa53e87dfa61ec1cab
rugved0102 <rugvedjunghare@gmail.com>
2025-06-07 22:03:08 +0530

    User component added

--- 07_reactRouter/src/main.jsx
+++ 07_reactRouter/src/main.jsx
@@ -7,6 +7,7 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
 
 const router = createBrowserRouter([
   {
@@ -24,6 +25,10 @@ const router = createBrowserRouter([
       {
         path: "contact",
         element: <Contact/>
+      },
+      {
+        path: "user/:userid",
+        element: <User/>
       }
     ]
   }
