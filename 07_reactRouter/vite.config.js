fcdac89795f0348e51dbbf82bba9e5eafe37b097
rugved0102 <rugvedjunghare@gmail.com>
2025-06-21 16:00:32 +0530

    redux project added

--- 07_reactRouter/vite.config.js
+++ 07_reactRouter/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/React/07_reactRouter/',
+  // base: '/React/07_reactRouter/',
   plugins: [react()],
 })
--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -1,10 +1,8 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos: [{id: 1, text: "Hello World"}]
+    todos: [{id: 1, text: "Hello World", isEditable: false, liveEditText: ""}]
 }
-
-
 
 // Slice ek reducer ka version hai, reducer kuch nhi bass ek functionality hai
 export const todoSlice = createSlice({
@@ -14,18 +12,42 @@ export const todoSlice = createSlice({
         addTodo: (state, action) => {
             const todo = {
                 id: nanoid(),
-                text: action.payload  // payload ek object hai
+                text: action.payload,  // payload ek object hai // The text comes from the user
+                isEditable: false,
+                liveEditText: ''
             }
             state.todos.push(todo)
         }, // state yaha pe initial jo state hai uska access deta hai agar initially 10 todo hai apne pass to vo, vo dega. Aur removeTodo ke liye kuch id lagagi to vo values action mese melegi
         removeTodo: (state, action) => {
             state.todos = state.todos.filter((todo) => todo.id != action.payload)
         },
+        toggleEditable: (state, action) => {
+            const todo = state.todos.find(todo => todo.id === action.payload)
+            if(!todo.isEditable) {
+                
+                todo.liveEditText = todo.text
+            }
+            todo.isEditable = !todo.isEditable
+        },
+        editLiveText: (state, action) => {
+            const {id, newText} = action.payload
+            const todo = state.todos.find(todo => todo.id == id)
+            if(todo) {
+                todo.liveEditText = newText
+            }
+        },
+        commitEdit: (state, action) => {
+            const todo = state.todos.find(todo => todo.id === action.payload)
+            if(todo) {
+                todo.text = todo.liveEditText
+                todo.isEditable = false;
+            }
+        }
     }
     // context api me sirf func declaration likhte the yaha redux toolkit me defination aur declaration dono likhe jate hai
 
 })
 
-export const { addTodo, removeTodo } = todoSlice.actions
+export const { addTodo, removeTodo, toggleEditable, editLiveText, commitEdit } = todoSlice.actions
 
 export default todoSlice.reducer
